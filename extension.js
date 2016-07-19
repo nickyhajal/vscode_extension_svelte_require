529955b5f0662d60ac1fd55b4a4cd5c5d92db6c0
milkmidi <milkmidi@medialand.tw>
2016-07-19 20:57:27 +0800

    update package display name

--- extension.js
+++ extension.js
@@ -1,8 +1,13 @@
 const vscode = require( 'vscode' );
 const path = require('path');
 const wwwRoot = vscode.workspace.rootPath;
+
+
 function activate( context ) {
     console.log( 'Congratulations, your extension "vs-code-quick-require" is now active!' );
+
+    // var test = "../../src/milkmidi.js";
+    // console.log( test.split('../') );
 
     var disposable = vscode.commands.registerCommand( 'extension.quickRequire', function () {
         var edit = vscode.window.activeTextEditor;
@@ -13,7 +18,7 @@ function activate( context ) {
             var items = [];
             for ( var i = 0; i < result.length; i++ ) {
                 var o = result[i];
-                if ( o.fsPath.indexOf( 'gulpfile.js' ) != -1 || o.fsPath.indexOf( 'dist.js' ) != -1) {
+                if ( o.fsPath.indexOf( 'gulpfile.js' ) != -1 || o.fsPath.indexOf( 'dist' ) != -1) {
                     continue;
                 }
                 items.push( {
@@ -23,16 +28,28 @@ function activate( context ) {
                 });
             }      
             vscode.window.showQuickPick( items, { placeHolder: 'select file' }).then(( value ) => {
-                // console.log(edit.document.fileName);                                
-                // console.log( value.fsPath );        
-                var relativePath = path.relative(edit.document.fileName, value.fsPath);
-                relativePath = relativePath.replace(/\\/g,"/"); 
+                
+                if( !value ){
+                    return;
+                }
+                // console.log( edit.document.fileName);                                
+                // console.log( value.fsPath );
+                var dirName = path.dirname( edit.document.fileName );
+                // console.log('fName', fName );
+                var relativePath = path.relative( dirName, value.fsPath);
+                relativePath = relativePath.replace(/\\/g,"/");
+                // relativePath =relativePath.substr(3 , relativePath.length); 
                 var fileName = path.basename( value.fsPath, '.js' );
+                if ( relativePath.indexOf("../") == - 1) {
+                    relativePath = "./"+relativePath;
+                }
+                // console.log( relativePath );
+                var script = "var "+fileName+" = require(\""+relativePath+"\");\n";
+                // console.log( script );
                 edit.edit(( editBuilder ) => {
                     var position = new vscode.Position(0,0);
                     editBuilder.insert( position, "var "+fileName+" = require(\""+relativePath+"\");\n" );
                 });
-
             });
         });
     });
@@ -49,6 +66,9 @@ exports.deactivate = deactivate;
 /**
  * @param  {string} s
  */
-function findRelativePath( currentFile , requiteFile ) {
-    var c = currentFile.split('\\');
+function fixRelativePath( s  ) {
+    var arr = s.split('../');
+    if ( arr.length ) {
+        
+    }
 }
