483d155548acf7fb29365641991a7777669e1d46
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2017-01-08 21:02:37 +0100

    Fix a failure to import on the right line

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
-# 0.3
+# 0.3.2
+
+- FIX: an issue where it would put the import line below other code.
+
+# 0.3.1
 
 - Import vs require is now auto-detected using the first occurence in the file. When there are no occurences, it will ask.
 - If there is no line-break between the dependencies and the following code, one will be inserted.
--- src/get-position.js
+++ src/get-position.js
@@ -1,13 +1,22 @@
+const _ = require('lodash');
+
+function isRequire(line) {
+    return line.match(/require/) || line.match(/^import/);
+}
+
+function isCommentOrEmpty(line) {
+    return _.isEmpty(line) || line.match(/^\s*\/\//);
+}
+
 module.exports = function(codeBlock) {
     let candidate = 0;
 
     for (let i = 0; i < codeBlock.length; i += 1) {
         const line = codeBlock[i];
-        const lineIsRequire = line.match(/require/) || line.match(/^import/);
 
-        if (lineIsRequire) {
+        if (isRequire(line)) {
             candidate = i + 1;
-        } else if (i > 3) {
+        } else if (!isCommentOrEmpty(line)) {
             break;
         }
     }
