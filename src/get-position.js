2945e363b6ebfc3c61c2b9c539927a3c6571cbdd
Nicky Hajal <nhajal@gmail.com>
2017-04-07 12:23:54 -0500

    Insert absolute imports before relative imports

--- src/get-position.js
+++ src/get-position.js
@@ -8,13 +8,21 @@ function isCommentOrEmpty(line) {
     return _.isEmpty(line) || line.match(/^\s*\/\//) || line.match(/^\s*["']use strict["']/);
 }
 
-module.exports = function(codeBlock) {
+function isLocalRequire(line) {
+    return line.match(/require\([\s]?['|"][.|/]/) || line.match(/^import.*from\s['|"][.|/]/);
+}
+
+module.exports = function(codeBlock, placeWithExternals) {
     let candidate = 0;
 
     for (let i = 0; i < codeBlock.length; i += 1) {
         const line = codeBlock[i];
 
-        if (isRequire(line)) {
+        if (isRequire(line) && (
+            !placeWithExternals ||
+            (placeWithExternals && !isLocalRequire(line))
+           )
+        ) {
             candidate = i + 1;
         } else if (!isCommentOrEmpty(line)) {
             break;
