a61e71a8c55a70198d19c9e486eaf2dd94293cd7
Taoh Green <taohgreen@gmail.com>
2017-08-21 23:24:39 -0700

    add support for leading block comments

--- src/get-position.js
+++ src/get-position.js
@@ -24,13 +24,29 @@ function isNamedImportEnd(line) {
   return line.match(/^.*from\s['|"]/)
 }
 
+function isStartOfBlockComment(line) {
+  return line.match(/^\s*\/\*/)
+}
+
+function isEndOfBlockComment(line) {
+  return line.match(/^\s*\*\//)
+}
+
 module.exports = function(codeBlock, placeWithExternals) {
   let candidate = 0
   let findingNamedImportEnd = false
+  let findingBlockCommentEnd = false
+
   for (let i = 0; i < codeBlock.length; i++) {
     const line = codeBlock[i]
     if (findingNamedImportEnd) {
       if (isNamedImportEnd(line)) findingNamedImportEnd = false
+      candidate = i + 1
+    } else if (findingBlockCommentEnd) {
+      if (isEndOfBlockComment(line)) findingBlockCommentEnd = false
+      candidate = i + 1
+    } else if (isStartOfBlockComment(line) && !isEndOfBlockComment(line)) {
+      findingBlockCommentEnd = true
       candidate = i + 1
     } else if (
       isRequire(line) &&
