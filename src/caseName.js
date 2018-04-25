1e00cebfaeea53de813911f86f481d978915272c
Einar Löve <post@einarlove.com>
2018-04-25 10:49:38 +0200

    Support import variable names with subsequent uppercase characters
    This pull request makes the case conversion on variable names retain the capitalisation of words.
    ```js
    // old
    import XmlImporter from './XMLImporter'
    
    // new
    import XMLImporter from './XMLImporter'
    ```
    
    ```js
    // old
    import globalIdResolver from './globalIDResolver'
    
    // new
    import globalIDResolver from './globalIDResolver'
    ```

--- src/caseName.js
+++ src/caseName.js
@@ -1,7 +1,11 @@
 const _ = require('lodash')
 
 module.exports = function(originalName) {
-  const camel = _.camelCase(originalName)
-  const fixedCase = originalName.slice(0, 1) + camel.slice(1)
-  return fixedCase
+  const caseName = _.startCase(originalName).split(' ').join('')
+
+  if (/^[a-z]/.test(originalName)) {
+    return _.lowerFirst(caseName)
+  }
+
+  return caseName
 }
