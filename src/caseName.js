8367249704e39b896715fc1a1714712e0171f8fe
einarlove <post@einarlove.com>
2018-07-23 16:52:40 +0200

    Toggle new casing logic with node_require.preserveAcronymCase

--- src/caseName.js
+++ src/caseName.js
@@ -1,11 +1,17 @@
 const _ = require('lodash')
 
-module.exports = function(originalName) {
-  const caseName = _.startCase(originalName).split(' ').join('')
+module.exports = function(originalName, preserveAcronymCase) {
+  if (preserveAcronymCase) {
+    const caseName = _.startCase(originalName)
+      .split(' ')
+      .join('')
 
-  if (/^[a-z]/.test(originalName)) {
-    return _.lowerFirst(caseName)
+    if (/^[a-z]/.test(originalName)) {
+      return _.lowerFirst(caseName)
+    }
+
+    return caseName
   }
 
-  return caseName
+  return originalName.slice(0, 1) + _.camelCase(originalName).slice(1)
 }
